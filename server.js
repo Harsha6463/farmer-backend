a277cbe3c8c209792963dd2ac63c6bb854813f52
Harsha6463 <157800250+Harsha6463@users.noreply.github.com>
2025-05-29 10:22:55 +0530

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin:" http://localhost:3000"
+  origin:" https://farmer-frontend-five.vercel.app/"
 }));
 app.use(express.json());
 
